5b3cbed9cc5e5eb266248d64ad39a8de53a885b6
doganekici <doganekici26@gmail.com>
2019-11-05 10:41:08 +0300

    Screens improvements

---
+++ Appcircle/ViewController2.h
@@ -0,0 +1,19 @@
+//
+//  ViewController2.h
+//  Appcircle
+//
+//  Created by Dogan Ekici on 5.11.2019.
+//  Copyright © 2019 Dogan Ekici. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ViewController2 : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *lblVersion;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Appcircle/ViewController2.m
@@ -0,0 +1,34 @@
+//
+//  ViewController2.m
+//  Appcircle
+//
+//  Created by Dogan Ekici on 5.11.2019.
+//  Copyright © 2019 Dogan Ekici. All rights reserved.
+//
+
+#import "ViewController2.h"
+
+@interface ViewController2 ()
+
+@end
+
+@implementation ViewController2
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+   
+    self.lblVersion.text =  [NSString stringWithFormat:@"v%@",[[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"]];;
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
