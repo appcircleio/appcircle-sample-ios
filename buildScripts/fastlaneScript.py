2b9b25612b03e70d010308b1d8b4fde0b3ad1f86
boztopuz <boztopuz@gmail.com>
2023-12-05 19:15:28 +0300

    added python script for fastlane

---
+++ buildScripts/fastlaneScript.py
@@ -0,0 +1,15 @@
+import requests
+
+def download_file(url, destination):
+    response = requests.get(url, stream=True)
+    if response.status_code == 200:
+        with open(destination, 'wb') as file:
+            for chunk in response.iter_content(1024):
+                file.write(chunk)
+        print(f"File dowloaded pyhton: {destination}")
+    else:
+        print(f"File can not download pyhton: {url}")
+
+output_dir = os.getenv('AC_OUTPUT_DIR')
+url = "https://raw.githubusercontent.com/opencv/opencv/master/README.md"
+download_file(url, f"{output_dir}/exampleFile.md")
