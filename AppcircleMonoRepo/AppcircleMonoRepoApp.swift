470820648a9b10884ba21f222a3908ea3bae0d1c
boztopuz <boztopuz@gmail.com>
2024-05-31 10:19:02 +0300

    Added mono repo targets

---
+++ AppcircleMonoRepo/AppcircleMonoRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppcircleMonoRepoApp.swift
+//  AppcircleMonoRepo
+//
+//  Created by Burak Öztopuz on 31.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppcircleMonoRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppcircleMonoRepo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppcircleMonoRepo
+//
+//  Created by Burak Öztopuz on 31.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
