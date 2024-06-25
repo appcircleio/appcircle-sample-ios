81132c0d5bddb566e683a43f1acd5e3a1c7e0cde
boztopuz <boztopuz@gmail.com>
2024-06-25 16:25:21 +0300

    Added highSizeImages for heavy binary

--- Appcircle/ContentView.swift
+++ Appcircle/ContentView.swift
@@ -22,18 +22,30 @@ struct FizzBuzzView: View {
 
 struct ContentView: View {
     @State var numberString: String = ""
+    
+    let images = ["image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8", "image9","image1copy", "image2copy", "image3copy", "image4copy", "image5copy", "image6copy", "image7copy", "image8copy", "image9copy", "image1copy copy", "image2copy copy", "image3copy copy", "image4copy copy", "image5copy copy", "image6copy copy", "image7copy copy", "image8copy copy", "image9copy copy"]
     var body: some View {
 
-        Text("Appcircle")
-        Image("Logo")
-            .resizable()
-            .frame(width: 64, height: 64)
-
-        Form {
-            TextField(text: $numberString, prompt: Text("Enter a number")) {
-                Text("Number")
+        VStack{
+            Text("Appcircle")
+    //        Image("Logo")
+    //            .resizable()
+    //            .frame(width: 64, height: 64)
+    //
+    //        Form {
+    //            TextField(text: $numberString, prompt: Text("Enter a number")) {
+    //                Text("Number")
+    //            }
+    //            FizzBuzzView(number: Int(numberString) ?? 0)
+    //        }
+            
+            ScrollView {
+                ForEach(images, id:\.self) { image in
+                    Image(image)
+                        .resizable()
+                        .frame(width: 200, height: 300)
+                }
             }
-            FizzBuzzView(number: Int(numberString) ?? 0)
         }
 
     }
